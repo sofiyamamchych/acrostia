606221a62ee5fe6413b546b084cc3faff3de1589
sofiyamamchych <sofiyamamchich@gmail.com>
2019-04-06 20:30:26 +0300

    header draft

---
+++ app.js
@@ -0,0 +1,10 @@
+  $(document).ready(function(){
+    $('.hero__carousel__main').slick({
+        speed: 300,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: true,
+        prevArrow: $('.hero__carousel_prev'),
+        nextArrow: $('.hero__carousel_next'),
+    });
+  });
