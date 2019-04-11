86b5e783a70f8ecd9a23d7ed4528b08251de7467
sofiyamamchych <sofiyamamchich@gmail.com>
2019-04-11 21:59:20 +0300

    script moved to app.js, overlay div moved down to body end

--- app.js
+++ app.js
@@ -11,5 +11,11 @@
     });
   });
 
-  
+function openNav() {
+  document.getElementById("myNav").style.width = "100%";
+}
 
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+
+}
