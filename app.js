f45aa9733f1316ef033c14a2cff5c93be30d5c29
sofiyamamchych <sofiyamamchich@gmail.com>
2019-04-15 19:28:21 +0300

    hover for teammember block done- js

--- app.js
+++ app.js
@@ -21,14 +21,34 @@ function closeNav() {
 }
 
 function redImg(container) {
-    // console.log('container: ', container);
     var photo = $(container).find('.teammember__photo__img');
-    console.log('src: ', photo.attr('src'));
     photo.attr('src', 'img/team-member-active.svg');
+    var fbIcon = $(container).find('.social__fb');
+    fbIcon.css({'background':'#4f689e'});
+    var twIcon = $(container).find('.social__tw');
+    twIcon.css({'background':'#74c7d5'});
+    var gplusIcon = $(container).find('.social__gplus');
+    gplusIcon.css({'background':'#df5c64'});
+    var lnIcon = $(container).find('.social__ln');
+    lnIcon.css({'background':'#3e61af'});
+    var imgBorder = $(container).find('.teammember__photo');
+    imgBorder.css({'border': '4px solid #df5c64'});
+    container.style.background = "#524350";
 }
 
 function normalImg(container) {
     var photo = $(container).find('.teammember__photo__img');
-    console.log('src: ', photo.attr('src'));
-    photo.attr('src', 'img/team-member.svg'); 
+    photo.attr('src', 'img/team-member.svg');
+    var fbIcon = $(container).find('.social__fb');
+    fbIcon.css({'background':'#a1859e'});
+    var twIcon = $(container).find('.social__tw');
+    twIcon.css({'background':'#a1859e'});
+    var gplusIcon = $(container).find('.social__gplus');
+    gplusIcon.css({'background':'#a1859e'});
+    var lnIcon = $(container).find('.social__ln');
+    lnIcon.css({'background':'#a1859e'});
+    var imgBorder = $(container).find('.teammember__photo');
+    imgBorder.css({'border': '4px solid #a1859e'});
+    container.style.background = "#5c4b5a";
+
 }
